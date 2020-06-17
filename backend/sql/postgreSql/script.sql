edc4d5802010b04e93959cd51019c17a17e6db81
Yassine AZIMANI <yassine.azimani@hotmail.fr>
2020-06-17 18:05:23 +0200

    Implementation of singleton JDBC + Compatibility with PostgreSQL 12.3 and more

---
+++ backend/sql/postgreSql/script.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE EducationProject;
+
+CREATE TABLE matiere (
+	id SERIAL PRIMARY KEY NOT NULL,
+	nom VARCHAR(40) NOT NULL DEFAULT '',
+	couleurFond VARCHAR(15) NOT NULL DEFAULT '',
+	couleurPolice VARCHAR(15) NOT NULL DEFAULT '',
+	volumeHoraire VARCHAR(5) NULL DEFAULT '',
+	description VARCHAR(255) NULL DEFAULT '',
+	creationDate TIMESTAMP NOT NULL DEFAULT NOW(),
+	modificationDate TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+COMMENT ON COLUMN matiere.nom is 'C''est le nom de la matière.';
+COMMENT ON COLUMN matiere.couleurFond is 'C''est la couleur de fond de la matière.';
+COMMENT ON COLUMN matiere.couleurPolice is 'C''est la couleur de la police de la matière.';
+COMMENT ON COLUMN matiere.volumeHoraire is 'C''est le volume horaire d''une matière.';
+COMMENT ON COLUMN matiere.description is 'C''est la description d''une matière.';
